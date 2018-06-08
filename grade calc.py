4ed5ed1b1c87e91e695ff789886987b13bb0215e
Maya Patel <patelm34@coventry.ac.uk>
2018-06-08 23:35:56 +0100

    fixed error
    comment error meant repeating functions, these are now changed

--- grade calc.py
+++ grade calc.py
@@ -33,33 +33,6 @@ def getCredits():
         return credit
 
 
-def getGrade():
-    """METHOD TO GET INPUT FROM USER, TEST INPUT AND APPLY MATH TO FIND AMOUNT OF CREDITS GAINED PER MODULE"""
-    grade = float(input("input your grade: "))
-    
-    if grade > 100:
-        print(errormsg(1))
-    elif isinstance(grade,float)==False :
-        print(errormsg(2))
-    else:
-        credit= getCredits() #CALLS ANOTHER FUNCTION
-        val=(grade/100)*credit
-        return (val)
-    
-    
-def getCredits():
-    """METHOD USED TO OBTAIN MODULE CREDIT INFO AND UPDATE GLOBAL CREDIT COUNT"""
-    credit = int(input("input how many credits the module is worth: "))
-    if credit > 50:
-        print(errormsg(3))
-    elif isinstance(credit,int)==False :
-        print(errormsg(4))
-    else:
-        global creditTotal #GLOBAL VARIABLE USE, BAD PRACTISE BUT WORKS
-        creditTotal +=credit
-        return credit
-
-
 def findTotal(list):
     """METHOD WHICH SUMS TOGETHER THE GRADE VALUES DERIVED IN GET GRADE AND DIVIDES BY GLOBAL CREDIT TOTAL"""
     listTotal=sum(alist)
@@ -78,13 +51,18 @@ def errormsg(n):
         return("Error, module credit is never higher than 50")
     elif n ==4:
         return("Error module credit should be a whole number divisible by 5")
+    elif n ==5:
+        return("Zero value removed from list")
 
 def main():
     """FUNCTION HAS BASIC IMPLEMENTATION OF GET GRADES AND OTHER FUNCTIONS RUN IN A LOGICAL ORDER, OUTPUTS FINAL VALUE"""
     liLen= int(input("how many grades do you have?"))
     for i in range (liLen):
         value = getGrade()
-        alist.append(value)
+        if value > 0:
+            alist.append(value)
+        else:
+            print(errormsg(4))
 
     fin=findTotal(alist)
     print ("calculated working grade based on credits: "+"{:.2%}".format(fin))#OUTPUTS INFORMATION IN '100.00%' FORMAT
@@ -94,8 +72,37 @@ alist=[] #GLOBAL VARIABLE
 creditTotal=0 #GLOBAL VARIABLE
 
 main()    #RUN
+print (alist)
 
-"""def findTotal(list):
+"""
+def getGrade():
+    #METHOD TO GET INPUT FROM USER, TEST INPUT AND APPLY MATH TO FIND AMOUNT OF CREDITS GAINED PER MODULE
+    grade = float(input("input your grade: "))
+    
+    if grade > 100:
+        print(errormsg(1))
+    elif isinstance(grade,float)==False :
+        print(errormsg(2))
+    else:
+        credit= getCredits() #CALLS ANOTHER FUNCTION
+        val=(grade/100)*credit
+        return (val)
+    
+    
+def getCredits():
+    #METHOD USED TO OBTAIN MODULE CREDIT INFO AND UPDATE GLOBAL CREDIT COUNT"
+    credit = int(input("input how many credits the module is worth: "))
+    if credit > 50:
+        print(errormsg(3))
+    elif isinstance(credit,int)==False :
+        print(errormsg(4))
+    else:
+        global creditTotal #GLOBAL VARIABLE USE, BAD PRACTISE BUT WORKS
+        creditTotal +=credit
+        return credit
+
+
+def findTotal(list):
     #METHOD WHICH SUMS TOGETHER THE GRADE VALUES DERIVED IN GET GRADE AND DIVIDES BY GLOBAL CREDIT TOTAL
     listTotal=sum(alist)
     global creditTotal #GLOBAL VARIABLE
@@ -113,18 +120,13 @@ def errormsg(n):
         return("Error, module credit is never higher than 50")
     elif n ==4:
         return("Error module credit should be a whole number divisible by 5")
-    elif n ==5:
-        return("Zero value removed from list")
 
 def main():
     #FUNCTION HAS BASIC IMPLEMENTATION OF GET GRADES AND OTHER FUNCTIONS RUN IN A LOGICAL ORDER, OUTPUTS FINAL VALUE
     liLen= int(input("how many grades do you have?"))
     for i in range (liLen):
         value = getGrade()
-        if value > 0:
-            alist.append(value)
-        else:
-            errormsg(4)
+        alist.append(value)
 
     fin=findTotal(alist)
     print ("calculated working grade based on credits: "+"{:.2%}".format(fin))#OUTPUTS INFORMATION IN '100.00%' FORMAT
@@ -134,4 +136,4 @@ alist=[] #GLOBAL VARIABLE
 creditTotal=0 #GLOBAL VARIABLE
 
 main()    #RUN
-print (alist)"""
+"""
