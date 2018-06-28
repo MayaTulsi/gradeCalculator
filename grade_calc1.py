781c236cf9827ed750464018c1f2bd1d1e6280ce
Maya Patel <patelm34@coventry.ac.uk>
2018-06-28 16:12:30 +0100

    commented code
    added a lot of comments to aid understanding of the code

--- grade_calc1.py
+++ grade_calc1.py
@@ -1,11 +1,12 @@
 # import os
 # grade calculator second prototype bases on learning from first prototype
 
-def user_input(n):
+
+def user_input(n):      # function to take pieces of data from user with which to calculate final percentage.
     if n is 1:
         number_of_modules = (input("Please input the number of modules you wish to find the average of: "))
-        mod_num = error_check(1, number_of_modules, int, 25, 1, 0)
-        return mod_num
+        mod_num = error_check(1, number_of_modules, int, 25, 1, 0)      # error check inputted value
+        return mod_num              # return checked value to main function
 
     elif n is 2:
         grade = input("Please input your grade: ")
@@ -18,31 +19,29 @@ def user_input(n):
         return int(checked_credit)
 
 
-def error_check(n, value, type, max, min, mod):
+def error_check(n, value, type, max, min, mod):     # various parameters used to test value for suitability
     try:
-        truevalue = type(value)
+        truevalue = type(value)     # if value is of the expected type it is assigned to the variable 'truevalue'
 
-        if truevalue > max or truevalue <= min:
-            error_message(2, type, min, max)
-            return user_input(n)
+        if truevalue > max or truevalue <= min:     # checks value is within specified size limitations (min, max)
+            error_message(2, type, min, max)        # calls error function to print appropriate error message
+            return user_input(n)        # user is asked for input of same information(n) again
 
-        elif mod is not 0:
-            if truevalue % 5 != 0:
+        elif mod is not 0:              # only runs when mod is not 0, as only credit value requires this check
+            if truevalue % 5 != 0:      # if value is not divisible by 5 error message function is called
                 error_message(3, type, min, max)
-                return user_input(n)
+                return user_input(n)    # user is asked for input of same information(n) again
             else:
-                return truevalue
-
+                return truevalue    # if mod is divisible by 5 truevalue is returned this is used for calculation
         else:
-            return truevalue
+            return truevalue        # if value is of the correct type and is within size limits truevalue is returned
 
-    except ValueError:
+    except ValueError:              # if type is not that which is expected error message is displayed
         error_message(1, type, min, max)
-        return user_input(n)
+        return user_input(n)        # user is asked for input of same information(n) again
 
 
-
-def error_message(n,typ,min,max):
+def error_message(n,typ,min,max):   # error messages used in error_check function to nudge user to input correct value
     if n is 1:
         print(" Value error please input data in the " + str(typ) + " format")
     elif n is 2:
@@ -52,18 +51,18 @@ def error_message(n,typ,min,max):
 
 
 def main():
-    credit_tot = 0
-    alist = []
+    credit_tot = 0      # credit total set to 0
+    alist = []          # empty list
 
-    modulenumber = user_input(1)
-    for i in range(modulenumber):
+    modulenumber = user_input(1)    # calls for first user input,
+    for i in range(modulenumber):   # runs below code for number of modules specified in first input
         grade = user_input(2)
         cred = user_input(3)
-        alist.append((grade/100)*cred)
-        credit_tot += cred
+        alist.append((grade/100)*cred) # finds weighted grade depending on credit value and adds to list
+        credit_tot += cred          # adds credit value to credit total, this is important for finding final average
 
-    litotal = sum(alist)
-    res = litotal/credit_tot
-    print("Calculated working grade based on credits: " + "{:.2%}".format(res))
+    litotal = sum(alist)            # sum of weighted grade values in list
+    res = litotal/credit_tot        # finds final weighted result
+    print("Calculated working grade based on credits: " + "{:.2%}".format(res)) # prints to 2 decimal places
 
 main()
